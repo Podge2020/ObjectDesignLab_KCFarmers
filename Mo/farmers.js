3a2033cd6b13c691e3360cc5c301ab7e6f1a3caf
queencoder1 <71669199+queencoder1@users.noreply.github.com>
2020-11-11 15:36:17 -0600

    updated farmers.js

---
+++ Mo/farmers.js
@@ -0,0 +1,16 @@
+class Event {
+    constructor (location, date, time=null, details){
+      this.location = zipcode;
+      this.date = date;
+      this.time = time;
+      this.details = details; 
+    };
+    addEvent(){
+
+    };
+    deleteEvent(){
+
+    };
+  };
+  
+  let date = new Date()
