b3510dc88a3d3748c04f9ae3871616a8dc16d0b7
Podge2020 <71773171+Podge2020@users.noreply.github.com>
2020-12-01 15:41:59 -0600

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,5 +1,6 @@
 var Comment = new Schema({​​​​​​​ ... }​​​​​​​);
 
+//Forum Schema - includes title of question, answer content, comments, and users 
 var Forum = new Schema({​​​​​​​
     title: {​​​​​​​type: String}​​​​​​​,
     content: {​​​​​​​type: String}​​​​​​​,
@@ -7,11 +8,37 @@ var Forum = new Schema({​​​​​​​
     attendees: [User]
 }​​​​​​​);
 
+//Users Schema - includes name, email, and forum properties 
 var User = new Schema({​​​​​​​
     name: {​​​​​​​type: String}​​​​​​​,
     email: {​​​​​​​type: String}​​​​​​​,
-    forums: [Forum]
+    forums: [Forum], 
+    saves: {type: Array}
 }​​​​​​​);
 
+//creates MODEL to hold users in Account object 
 var Account = mongoose.model('Account', User);
+
+//Calendar Schema - inclues event title, location, time of event, description, dates, and who
+var Calendar = new Schema({
+        title: {​​​​​​​type: String}​​​​​​​,
+        location: {​​​​​​​type: String}​​​​​​​,
+        time: {​​​​​​​type: Number}​​​​​​​,
+        description: {​​​​​​​type: String}​​​​​​​,
+        dates: {​​​​​​​type: Date}​​​​​​​,
+        who: {​​​​​​​type: String}​​​​​​,
+
+
+//Producer Schema - includes name, 
+var Producer = new Schema({
+    name: {​​​​​​​type: String}​​​​​​​,
+    location: {​​​​​​​type: String}​​​​​​​,
+    produce: {​​​​​​​type: Array}​​​​​​​,
+})        
+
+/* Also in the code comments, 
+for at least one Schema per person, 
+discuss what the pros and cons 
+of developing the documents in XML in a r
+elational database vs JSON/BSON in MongoDB would be/are. */
 
