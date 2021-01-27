ac0541536b1a52d489e2b03d0ffe69f3a283ca08
Podge2020 <71773171+Podge2020@users.noreply.github.com>
2021-01-27 13:17:23 -0600

    API calls
    part I in client JS, still requires index.html buttons to be made and attached

--- Paige/capstone/categoryCards.js
+++ Paige/capstone/categoryCards.js
@@ -11,3 +11,140 @@
 /producers/honey
 /producers/speciality 
 */ 
+
+async function getMeat(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/meat", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
+
+async function getProduce(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/produce", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
+
+async function getDairy(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/dairy", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+
+}; 
+
+async function getEggs(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/eggs", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
+
+async function getHerbs(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/herbs", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
+
+async function getHoney(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/honey", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
+
+async function getMushroom(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/mushroom", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
+
+async function getSpeciality(){
+    let requestOptions = {
+        method: "GET",
+        headers : { "Content-Type": "application/json"} 
+    }
+
+    const response = await fetch("/producer/speciality", requestOptions); 
+
+    //traverses the response object and pulls out the body content in the form of JSON 
+    const body = await response.json(); 
+    if (response.status != 200){
+       throw Error(body.message); 
+    }
+
+    return body; 
+}; 
